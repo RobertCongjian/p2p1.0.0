522031a3b75a910feccc7048d3aecce119344b44
dream_glb <2523214528_glb@qq.com>
2019-08-22 21:46:05 +0800

    second commit

--- core/src/main/java/com/gxa/p2p/common/service/ILoginInfoService.java
+++ core/src/main/java/com/gxa/p2p/common/service/ILoginInfoService.java
@@ -2,10 +2,12 @@ package com.gxa.p2p.common.service;
 
 import com.gxa.p2p.common.domain.LoginInfo;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface ILoginInfoService {
     int checkUsername(String username);
 
     void register(String username, String password);
 
-    LoginInfo login(String username, String password,int usertype);
+    LoginInfo login(String username, String password, int usertype, HttpServletRequest request);
 }
